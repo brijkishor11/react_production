bdf83525ef8460381f164d6147501120777e5c80
Brijkishor <brijkishor3094@gmail.com>
2020-01-07 15:47:14 +0530

    Updates

---
+++ precache-manifest.f1fa94d3b636743a9af6291b4da4cbda.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7760fe6e8c66a370aa3369c9ee098db",
+    "url": "/github.io/react_production/index.html"
+  },
+  {
+    "revision": "21bb75d46b884713d566",
+    "url": "/github.io/react_production/static/css/main.330e9c76.chunk.css"
+  },
+  {
+    "revision": "5d6e2ea0da5161fc9595",
+    "url": "/github.io/react_production/static/js/2.3bdef7b9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github.io/react_production/static/js/2.3bdef7b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21bb75d46b884713d566",
+    "url": "/github.io/react_production/static/js/main.b3151636.chunk.js"
+  },
+  {
+    "revision": "331c3842c63e6d7d48e1",
+    "url": "/github.io/react_production/static/js/runtime-main.37b4f9af.js"
+  },
+  {
+    "revision": "8315e9a332f283d20f766cfc82c11752",
+    "url": "/github.io/react_production/static/media/amul_ghee.8315e9a3.jpg"
+  },
+  {
+    "revision": "c8cb1af3aa44c03df119646047a6d0a3",
+    "url": "/github.io/react_production/static/media/fortune_rice.c8cb1af3.jpg"
+  },
+  {
+    "revision": "6316920654be8f91e48d7bd628a80465",
+    "url": "/github.io/react_production/static/media/mother_dairy.63169206.jpg"
+  },
+  {
+    "revision": "eecf1cad1471e7c404b346150dd2defb",
+    "url": "/github.io/react_production/static/media/patanjali_shampoo.eecf1cad.jpg"
+  }
+]);
